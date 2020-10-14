162c8c7cbd933a54d560f0b7e6f4ddd785e1b8aa
dikushan00 <dikushant@gmail.com>
2020-10-14 17:03:38 +0600

    Gatsby + contentful

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,6 @@ import React from "react"
 import { graphql, Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = ({data}) => (
@@ -12,11 +11,7 @@ const IndexPage = ({data}) => (
     <p>{data.contentfulArticle.img.description}</p>
       <img src={data.contentfulArticle.img.resolutions.src} alt=""/>
     <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
     <Link to="/page-2/">Go to page 2</Link> <br />
     <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
   </Layout>
